709190f02d463cf10d4d70a10186c48b8010395d
PajarAland <162042636+PajarAland@users.noreply.github.com>
2024-04-14 23:52:46 +0700

    menyelesaikan program dengan runtime configuration

--- obj/Debug/net8.0/tpmod8_1302223146.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpmod8_1302223146.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmod8_1302223146")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97218dbbf0f5062e315bf08f609623beaf865d79")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmod8_1302223146")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmod8_1302223146")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
